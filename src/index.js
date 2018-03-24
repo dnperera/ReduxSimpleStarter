f26e049d8413016464e7d2c40bdef830b4f8bffb
Dayan Perera <dayannp@gmail.com>
2018-03-23 19:21:18 -0700

    create component files & define state for the search_bar compoonent

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar from './components/search_bar';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAWyKjhHy4ykHnfTTfNKpBdwUi3F_5tIg4'; // youtube key
+//Create a component.
+const App = () => {
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//Take above jsx component and add or render to the DOM
+//
+ReactDOM.render(<App />, document.querySelector('.container'));
