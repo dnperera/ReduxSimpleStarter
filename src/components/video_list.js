e857633897c83044c168940f05576c91d5d8530a
Dayan Perera <dayannp@gmail.com>
2018-03-24 08:39:27 -0700

    add video list and video detail components

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -4,7 +4,7 @@ import VideoListItem from './video_list_item';
 const VideoList = props => {
 	// functional component
 	const list = props.videos.map((video, index) => {
-		return <VideoListItem video={video} key={index} />;
+		return <VideoListItem video={video} key={video.etag} />;
 	});
 
 	return <ul className="col-md-4 list-group">{list}</ul>;
